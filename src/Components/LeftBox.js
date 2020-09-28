1295de2bd76be86a5957a2e594578328ed1b9615
skfh5970841 <leechangwan1012@gmail.com>
2020-09-28 10:18:51 +0900

    logo added

--- src/Components/LeftBox.js
+++ src/Components/LeftBox.js
@@ -1,31 +1,34 @@
 import React from "react";
 import "../Style/LeftBox.css";
+import logo from "../img/logo.png";
 
 function LeftBox() {
 	return (
-		<div className="leftbox">
-			<button className="newPlan">
+		<div className="allBox">
+			<img src={logo} className="logo" />
+			<div className="leftbox">
 				<i className="fas fa-plus fa-3x button-icon"></i>
 				<span className="button-text">새로 만들기</span>
-			</button>
-			<ul class="plans rel-plan">
-				<span className="plan-text">최근 수정한 계획</span>
-				<li className="plan">haha-수정된 날짜</li>
-				<li className="plan">hehe</li>
-				<li className="plan">hoho</li>
-			</ul>
-			<ul class="plans imp-plan">
-				<span className="plan-text">중요 표시한 계획</span>
-				<li className="plan">haha</li>
-				<li className="plan">hehe</li>
-				<li className="plan">hoho</li>
-			</ul>
-			<ul class="plans all-plan">
-				<span className="plan-text">모든 계획</span>
-				<li className="plan">haha</li>
-				<li className="plan">hehe</li>
-				<li className="plan">hoho</li>
-			</ul>
+				<button className="newPlan"></button>
+				<ul class="plans rel-plan">
+					<span className="plan-text">최근 수정한 계획</span>
+					<li className="plan plan-top">haha-수정된 날짜</li>
+					<li className="plan">hehe</li>
+					<li className="plan">hoho</li>
+				</ul>
+				<ul class="plans imp-plan">
+					<span className="plan-text">중요 표시한 계획</span>
+					<li className="plan plan-top">haha</li>
+					<li className="plan">hehe</li>
+					<li className="plan">hoho</li>
+				</ul>
+				<ul class="plans all-plan">
+					<span className="plan-text">모든 계획</span>
+					<li className="plan plan-top">haha</li>
+					<li className="plan">hehe</li>
+					<li className="plan">hoho</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
