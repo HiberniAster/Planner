11fbdde38cace7752aa8c8744b44ab5119e81af6
skfh5970841 <leechangwan1012@gmail.com>
2020-10-26 22:49:26 +0900

    login redux store created

--- src/store.js
+++ src/store.js
@@ -1,7 +1,38 @@
 import { createStore } from "redux";
 
-const reducer = (state = [], action) => {};
+const LOGIN = "LOGIN";
+const LOGOUT = "LOGOUT";
+
+const login = (isLogined) => {
+  return {
+    type: LOGIN,
+    isLogined: isLogined,
+  };
+};
+
+const logout = () => {
+  return {
+    type: LOGOUT,
+    isLogined: false,
+  };
+};
+
+const reducer = (state = false, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return action.isLogined;
+    case LOGOUT:
+      return false;
+    default:
+      return state;
+  }
+};
 
 const store = createStore(reducer);
+
+export const actionCreators = {
+  login,
+  logout,
+};
 
 export default store;
